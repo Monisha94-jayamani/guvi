50545d7152d18eb9257314253243715e134265f5
Monisha94-jayamani <60962502+Monisha94-jayamani@users.noreply.github.com>
2020-02-15 11:27:47 +0530

    Create add-to-one-decimal-value.py

---
+++ codekata/absolute-beginner/add-to-one-decimal-value.py
@@ -0,0 +1,8 @@
+#you are given Two Numbers, A and B. If C = A + B. Find C.
+#Note: Round off the output to a single decimal place.
+#enter the input
+A=float(input(""))
+B=float(input(""))
+#print c before round off
+C=round(A+B,1)
+print(C)
