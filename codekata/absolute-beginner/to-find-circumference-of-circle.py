2f8f7d1b34d9fa8ae5fc00f2fea1cfd7c03ac31b
Monisha94-jayamani <60962502+Monisha94-jayamani@users.noreply.github.com>
2020-02-15 13:30:31 +0530

    Create to-find-circumference-of-circle.py

---
+++ codekata/absolute-beginner/to-find-circumference-of-circle.py
@@ -0,0 +1,9 @@
+#to find the circumference of circle
+#getting input as radius
+A=float(input(""))
+if(A>0):
+    #circumference operations
+    length=2*3.142*A
+    print(round(length,2))
+else:
+    print("Error")
