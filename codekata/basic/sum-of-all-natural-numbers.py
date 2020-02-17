1bfe7ba7172bb061a199d79ffef078e48c5ce471
Monisha94-jayamani <60962502+Monisha94-jayamani@users.noreply.github.com>
2020-02-17 11:26:47 +0530

    Create sum-of-all-natural-numbers.py

---
+++ codekata/basic/sum-of-all-natural-numbers.py
@@ -0,0 +1,11 @@
+#program to find sum of natural numbers
+#func to sum of all numbers
+def sumofnatural():
+    K=int(input(""))
+    sum=0
+    for i in range(1,K+1):
+        sum=sum+i
+    return sum
+result=sumofnatural()
+print(result)
+	
