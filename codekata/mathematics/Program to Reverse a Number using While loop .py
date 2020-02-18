9be3a41a495ebe116df69e2ad6a2d550189bb797
Monisha94-jayamani <60962502+Monisha94-jayamani@users.noreply.github.com>
2020-02-18 19:46:32 +0530

    Create Program to Reverse a Number using While loop .py

---
+++ codekata/mathematics/Program to Reverse a Number using While loop .py
@@ -0,0 +1,9 @@
+#Program to Reverse a Number using While loop   
+#getting input
+Number = int(input(""))    
+Reverse = 0    
+while(Number > 0):    
+    Reminder = Number %10    
+    Reverse = (Reverse *10) + Reminder    
+    Number = Number //10    
+print(Reverse)   
