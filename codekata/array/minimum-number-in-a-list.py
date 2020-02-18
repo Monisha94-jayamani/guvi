70559d0c07f8e59ec1a59c1b16d4e89b78c68b07
Monisha94-jayamani <60962502+Monisha94-jayamani@users.noreply.github.com>
2020-02-18 14:27:38 +0530

    Create minimum-number-in-a-list.py

---
+++ codekata/array/minimum-number-in-a-list.py
@@ -0,0 +1,8 @@
+#to find minimum number in a list
+#getting input
+N=int(input())
+#multiple input in single line
+n1=list(map(int,input().split()))
+#min func to get minimum value
+minimum=min(n1)
+print(minimum)
