9e7fb2cd6fa1e91961e1ae89eaeb03435276c6ef
Monisha94-jayamani <60962502+Monisha94-jayamani@users.noreply.github.com>
2020-02-20 09:55:56 +0530

    Update bank-application.py

--- Looping/bank-application.py
+++ Looping/bank-application.py
@@ -4,51 +4,63 @@ userbalance=5000
 hashdefinedpassword=hash(definedpassword)
 hashedusername=hash(username)
 #print(hashdefinedpassword)
+j=0
+N=1000
+while(j<N):
+    runtimeusername=input("enter the username:")
+    hashedruntimeuser=hash(runtimeusername)
+    if(hashedruntimeuser==hashedusername):
+        break
+        
+j=j+1
 i=0
 while(i<5):
-    runtimeusername=input("enter the username:")
+    #runtimeusername=input("enter the username:")
     runtimepassword=input("enter the password:")
-    hashedruntimeuser=hash(runtimeusername)
+    #hashedruntimeuser=hash(runtimeusername)
     hashedruntimepassword=hash(runtimepassword)
     #print(hashedruntimepassword)
     if(hashedusername== hashedruntimeuser):
         if(hashdefinedpassword==hashedruntimepassword):
-            print("ok")
+            print("logined successfully")
+        
             N=1000
             c=0
             while(i<=N):
-                tocheckbalance=input("you need to check balance: yes or no:")
-                if(tocheckbalance=="yes"):
+                print("1.balance")
+                print("2.deposit")
+                print("3.cash withdrawal")
+                a=int(input("enterthe choice:"))
+                if(a==1):
                     print("your balance is:",userbalance)
-                cashdeposit=input("the cash you need to deposit:")
-                if(cashdeposit=="yes"):
+                if(a==2):
                     userdeposit=int(input("enter the cash u need to deposit:"))
                     userbalance=userbalance+userdeposit
                     print("new balance",userbalance)
-                cash=input("you need to access the cash yes or no:")
-                if(cash=="yes"):
-                    requestingcash=int(input("enter the cash you need to accsessible:"))
+                if(a==3):
+                    requestingcash=int(input("enter the cash you need to cash withdrawal:"))
                     if(requestingcash>=userbalance and userbalance>=1000):
-                        print("not accsessible")
+                        print("insufficient balance")
                     else:
                         userbalance=userbalance-requestingcash  
                         print(requestingcash,"accessed")
                         print("userbalance",userbalance)
                         print("thank you for ur service")
-                    
+                   
                 #else:
                     #print("do u want to continue servies yes or no")
-                    
-                    
-                
+                   
+                   
+               
                 service=input("to do want continue yes or no:")
                 if(service=="no"):
                     exit()
                 c=c+1
     i=i+1            
 else:
-    
+   
     exit()    
-    
+
+   
 
 
