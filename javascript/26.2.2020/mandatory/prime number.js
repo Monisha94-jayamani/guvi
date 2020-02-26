614c4ee1500bd37268ab3f1df4fc29b9fe497f03
Monisha94-jayamani <60962502+Monisha94-jayamani@users.noreply.github.com>
2020-02-26 12:57:16 +0530

    Update prime number.js

--- javascript/26.2.2020/mandatory/prime number.js
+++ javascript/26.2.2020/mandatory/prime number.js
@@ -1,11 +1,8 @@
 const array = [1, 3, 2, 5, 10];
-const isPrime = num => {
+const prime = array.filter(num => {
   for (let i = 2; i < num; i++) {
     if (num % i === 0) return false;
   }
   return num !== 1;
-};
-
-
-const myPrimeArray = array.filter(element => isPrime(element));
-console.log(myPrimeArray);
+});
+console.log(prime);
