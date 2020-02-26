8622147ccbdf24322592b81768b291d92e45071d
Monisha94-jayamani <60962502+Monisha94-jayamani@users.noreply.github.com>
2020-02-26 12:38:21 +0530

    Create prime number.js

---
+++ javascript/26.2.2020/mandatory/prime number.js
@@ -0,0 +1,11 @@
+const array = [1, 3, 2, 5, 10];
+const isPrime = num => {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+  return num !== 1;
+};
+
+
+const myPrimeArray = array.filter(element => isPrime(element));
+console.log(myPrimeArray);
