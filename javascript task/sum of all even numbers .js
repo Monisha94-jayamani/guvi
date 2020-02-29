8a1acaa5651762c26c5f521e6f277488a43dcb73
Monisha94-jayamani <60962502+Monisha94-jayamani@users.noreply.github.com>
2020-02-29 07:35:19 +0530

    Create sum of all even numbers .js

---
+++ javascript task/sum of all even numbers .js
@@ -0,0 +1,17 @@
+const readline=require ('readline');
+const inp=readline.createInterface({
+  input:process.stdin
+});
+const userInput=[];
+var sum=0
+inp.on("line",(data)=>{
+  userInput.push(data)
+});
+inp.on("close",()=>{
+for(i=1;i<=userInput[0];i++){
+    if(i%2==0){
+        sum=sum+i;
+    }
+}
+console.log(sum+"");
+});
