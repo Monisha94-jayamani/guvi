3e401155349e935ddf3b484471df722098dc6f8a
Monisha94-jayamani <60962502+Monisha94-jayamani@users.noreply.github.com>
2020-04-08 14:13:25 +0530

    Create sort-the-characters-in-the-stringv.js

---
+++ javascript task/codekata/string/sort-the-characters-in-the-stringv.js
@@ -0,0 +1,13 @@
+const readline=require('readline')
+const imp=readline.createInterface({
+  input:process.stdin
+});
+var user=[];
+imp.on("line",(data)=>{
+  user.push(data)
+});
+imp.on("close",()=>{
+  var a=user[0].split("");
+  var ans=a.sort();
+  console.log(ans.join(""));
+});
