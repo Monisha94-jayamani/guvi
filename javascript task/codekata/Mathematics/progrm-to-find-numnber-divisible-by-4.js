023ab0eb1a238dac7dd00d95b5292f7a644a9131
Monisha94-jayamani <60962502+Monisha94-jayamani@users.noreply.github.com>
2020-04-09 19:24:22 +0530

    Create progrm-to-find-numnber-divisible-by-4.js

---
+++ javascript task/codekata/Mathematics/progrm-to-find-numnber-divisible-by-4.js
@@ -0,0 +1,23 @@
+const readline=require('readline');
+const inp=readline.createInterface({
+    input:process.stdin
+});
+var user=[];
+inp.on("line",(data)=>
+{
+  user.push(data);
+});
+inp.on("close",()=>{
+  var a=parseInt(user[0])
+  if(a%4==0)
+  {
+    console.log("YES");
+  }
+  else
+  {
+    console.log("NO");
+  }
+});
+
+  
+  
