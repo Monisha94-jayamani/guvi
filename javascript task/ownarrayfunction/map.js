a069137d9c013ef0df010d807ead4af3b6fb663b
Monisha94-jayamani <60962502+Monisha94-jayamani@users.noreply.github.com>
2020-05-07 13:04:42 +0530

    Update map.js

--- javascript task/ownarrayfunction/map.js
+++ javascript task/ownarrayfunction/map.js
@@ -1,36 +1,21 @@
-const readline=require('readline');
-const inp=readline.createInterface({
-    input:process.stdin
-});
-const n=[];
-inp.on('line',(data)=>{
-    n.push(data);
- });
-var k=[];
-var l=0;
-var sum=0;
-var res=[];
-var g;
-inp.on('close',()=>{
-    k=n[0].split(" ");
-function map1()
-{
- for(var i=0;i<k.length;i++)
- {  
-     l=0;
-     l=parseInt(k[i]*10);
-     //console.log(l);
-     res.push(l);
- }
- return g=res.join(" ");
- 
+const myevery=(array,callback)=>{
+    let c=0;
+    for(let i=0;i<array.length;i++)
+    {
+        if(callback(array[i])){
+            c+=1;
+        }
+    }
+    if(c===array.length)
+    
+        return true
+    
+    else
+    
+        return false
+    
+    
 }
-res1=map1();
-console.log(res1);
-});
-/*
-input:
-1 2 3 4 5
-output: 10 20 30 40 50
-exp:multiply the array elements in the list with 10 and print their values
-*/
+const predict=(x)=> x.length>3
+const data=["apple","bathh","catjk"]
+console.log(myevery(data,predict));
