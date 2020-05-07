f06aa2afdb6a94b11f60d0d1c6a875ba57593f2b
Monisha94-jayamani <60962502+Monisha94-jayamani@users.noreply.github.com>
2020-05-07 13:51:43 +0530

    Update concat.js

--- javascript task/ownarrayfunction/concat.js
+++ javascript task/ownarrayfunction/concat.js
@@ -1,31 +1,13 @@
-const readline=require('readline');
-const inp=readline.createInterface({
-  input:process.stdin
-});
-const n=[];
-inp.on('line',(data)=>{
-  n.push(data);
-});
-inp.on('close',()=>{
-  var s1=n[0];
-  var s2=n[1].split(" ");
-  var s;
-    function concat1(){
-      for(var i=0;i<s2.length;i++)
-      {
-          s1+=s2[i];
-      }
-      
-      return s1;
-  }
-  var res=concat1();
-  console.log(res);
-  
-});
-//output
-hi 
-good bye
-op:higoodbye
+const myconcat=(array,callback)=>{
     
-    
-    
+    for(let i=0;i<array.length;i++)
+    {
+           
+       callback(array[i])
+    }
+return s;
+}    
+var s="";
+const concatfunc=(x)=> s+=x;
+const inp=["hi","am","here"]
+console.log(myconcat(inp,concatfunc));
